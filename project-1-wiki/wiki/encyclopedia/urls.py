3aae4e05a154c702a524e79a78373064cdb2d852
Rayana Sales <rayanaasales@gmail.com>
2025-02-11 12:09:05 -0300

    Project 1: Search: Allow the user to type a query into the search box in the sidebar to search for an encyclopedia entry

--- project-1-wiki/wiki/encyclopedia/urls.py
+++ project-1-wiki/wiki/encyclopedia/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('wiki/<str:title>/', views.entry, name='entry'),
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("search", views.search, name="search"),
+    path("create", views.create, name="create"),
+    path("random", views.random_page, name="random"),
 ]
--- project-1-wiki/wiki/encyclopedia/views.py
+++ project-1-wiki/wiki/encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import util
 
 def index(request):
@@ -16,3 +16,23 @@ def entry(request, title):
         "title": title.replace("#", "").replace("*", ""),
         "content": entry_content.replace("#", "").replace("*", "")
     })
+
+def search(request):
+    query = request.GET.get('q', '')
+    if util.get_entry(query):
+        return redirect('entry', query)
+    else:
+        entries = util.list_entries()
+        results = [entry for entry in entries if query.lower() in entry.lower()]
+        return render(request, "encyclopedia/search.html", {
+            "query": query,
+            "results": results
+        })
+    
+def create(request):
+    # Add your create view logic here
+    return render(request, "encyclopedia/create.html")
+
+def random_page(request):
+    # Add your random page logic here
+    return render(request, "encyclopedia/random.html")
