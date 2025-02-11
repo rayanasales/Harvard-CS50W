b408e58e7eabe9ad50411621ef117b17cb9d3657
Rayana Sales <rayanaasales@gmail.com>
2025-02-10 23:35:35 -0300

    Project 1: Exercise specification 1: entry page

--- project-1-wiki/wiki/encyclopedia/urls.py
+++ project-1-wiki/wiki/encyclopedia/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('wiki/<str:title>/', views.entry, name='entry'),
+    path('wiki/<str:title>/', views.entry, name='entry'),
 ]
--- project-1-wiki/wiki/encyclopedia/views.py
+++ project-1-wiki/wiki/encyclopedia/views.py
@@ -6,13 +6,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
-# def entry(request, title):
-#     entry_content = util.get_entry(title)
-#     if entry_content is None:
-#         return render(request, "encyclopedia/error.html", {
-#             "message": "The requested page was not found."
-#         })
-#     return render(request, "encyclopedia/entry.html", {
-#         "title": title,
-#         "content": entry_content
-#     })
+def entry(request, title):
+    entry_content = util.get_entry(title)
+    if entry_content is None:
+        return render(request, "encyclopedia/error.html", {
+            "message": "The requested page was not found."
+        })
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": entry_content
+    })
