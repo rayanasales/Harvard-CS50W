1bd7619c2fb612e44e56e68ac920dc74ac8fdb32
Rayana Sales <rayanaasales@gmail.com>
2025-02-19 12:07:29 -0300

    Project 2: Setup Django Admin Interface

--- project-2-commerce/commerce/auctions/admin.py
+++ project-2-commerce/commerce/auctions/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import User, AuctionListing, Bid, Comment, Watchlist
+class AuctionListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'starting_bid', 'is_active', 'created_at')
+    list_filter = ('is_active', 'category', 'created_at')
+    search_fields = ('title', 'description', 'user__username')
+class BidAdmin(admin.ModelAdmin):
+    list_display = ('listing', 'user', 'amount', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('listing__title', 'user__username', 'amount')
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('listing', 'user', 'content', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('listing__title', 'user__username', 'content')
+class WatchlistAdmin(admin.ModelAdmin):
+    list_display = ('user', 'listing', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('user__username', 'listing__title')
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(AuctionListing, AuctionListingAdmin)
+admin.site.register(Bid, BidAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Watchlist, WatchlistAdmin)
