0e85543d9bdb6746dcf66cfe8849023b1ae64013
Rayana Sales <rayanaasales@gmail.com>
2025-03-05 12:14:35 -0300

    Project 5: Setup models and crud operations

--- project-5-habits-tracker/tracker/admin.py
+++ project-5-habits-tracker/tracker/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Habit, HabitCompletion
 
-# Register your models here.
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'start_date', 'time_of_day')
+    search_fields = ('name', 'description')
+    list_filter = ('start_date', 'user')
+    actions = ['delete_selected']
+
+class HabitCompletionAdmin(admin.ModelAdmin):
+    list_display = ('habit', 'date', 'completed')
+    list_filter = ('completed', 'date')
+    search_fields = ('habit__name',)
+    actions = ['toggle_completion']
+
+    def toggle_completion(self, request, queryset):
+        for completion in queryset:
+            completion.completed = not completion.completed
+            completion.save()
+    toggle_completion.short_description = "Toggle selected completions"
+
+admin.site.register(Habit, HabitAdmin)
+admin.site.register(HabitCompletion, HabitCompletionAdmin)
